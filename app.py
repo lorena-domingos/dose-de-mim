8fc3ea1f2baeb8987398d34dfe7d62117d027d5c
Lorena Mendes <212278877+loremd-dev@users.noreply.github.com>
2025-07-17 11:53:17 -0300

    - Criação de conexão com sqlite3 e teste de inserção de dados na tabela

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        texto = request.form.get("texto")
+        humor = request.form.get("humor")
+        tomou = 'remedio' in request.form
+
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO diario (texto, emoji) VALUES (?, ?)", (texto, humor))
+        cursor.execute("INSERT INTO remedio (tomou) VALUES (?)", (int(tomou),))
+        conn.commit()
+        conn.close()
     return render_template("index.html")
 
 if __name__ == "__main__":
