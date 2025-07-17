5c4522e5dcba47e7a198b13e13fc6d05bfb4652b
Lorena Mendes <212278877+loremd-dev@users.noreply.github.com>
2025-07-17 20:16:31 -0300

    - Adicionar modal de confirmação para deletar entrada e implementação do delete no "diario"

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sqlite3
 app = Flask(__name__)
 DATABASE = 'database.db'
 
+
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
@@ -11,11 +12,13 @@ def get_db():
         db.row_factory = sqlite3.Row
     return db
 
+
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -32,10 +35,19 @@ def index():
         return redirect(url_for("index"))
 
     db = get_db()
-    diarios = db.execute("SELECT data, texto, emoji FROM diario ORDER BY id DESC").fetchall()
+    diarios = db.execute("SELECT id, data, texto, emoji FROM diario ORDER BY id DESC").fetchall()
     remedios = db.execute("SELECT data, tomou FROM remedio ORDER BY id DESC").fetchall()
 
     return render_template("index.html", diarios=diarios, remedios=remedios)
+
+
+@app.route("/delete_diario/<int:id>")
+def delete_diario(id):
+    db = get_db()
+    db.execute("DELETE FROM diario WHERE id = ?", (id,))
+    db.commit()
+    return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
