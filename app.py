5461ae5d3c01dd00aa0b634a8feb1c18a4aec1eb
Lorena Mendes <212278877+loremd-dev@users.noreply.github.com>
2025-07-17 12:19:41 -0300

    - Ajustado conexão com a lib g pra impedir conflitos e exibição dos dados na tela principal

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, g
 import sqlite3
 
 app = Flask(__name__)
+DATABASE = 'database.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE, timeout=10)
+        db.row_factory = sqlite3.Row
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -10,13 +24,18 @@ def index():
         humor = request.form.get("humor")
         tomou = 'remedio' in request.form
 
-        conn = sqlite3.connect('database.db')
-        cursor = conn.cursor()
-        cursor.execute("INSERT INTO diario (texto, emoji) VALUES (?, ?)", (texto, humor))
-        cursor.execute("INSERT INTO remedio (tomou) VALUES (?)", (int(tomou),))
-        conn.commit()
-        conn.close()
-    return render_template("index.html")
+        db = get_db()
+        db.execute("INSERT INTO diario (texto, emoji) VALUES (?, ?)", (texto, humor))
+        db.execute("INSERT INTO remedio (tomou) VALUES (?)", (tomou,))
+        db.commit()
+
+        return redirect(url_for("index"))
+
+    db = get_db()
+    diarios = db.execute("SELECT data, texto, emoji FROM diario ORDER BY id DESC").fetchall()
+    remedios = db.execute("SELECT data, tomou FROM remedio ORDER BY id DESC").fetchall()
+
+    return render_template("index.html", diarios=diarios, remedios=remedios)
 
 if __name__ == "__main__":
     app.run(debug=True)
